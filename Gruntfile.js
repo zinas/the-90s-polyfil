a64bfafebd3019b592b30bd012c1c49d1eca2c8d
zinas <zinas.nikos@gmail.com>
2014-05-11 15:43:09 +0200

    added static server, finished welcome and under construction

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+      'connect': {
+          demo: {
+              options: {
+                  open: true,
+                  keepalive: true
+              }
+          }
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('server', ['connect']);
+}
